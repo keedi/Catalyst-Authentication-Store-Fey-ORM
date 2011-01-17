ae0e5534c3cee3f6039b7dd9b9370921f20b6fa6
Keedi Kim <keedi.k@gmail.com>
2011-01-17 19:35:48 +0900

    Implement C::A::Store::Fey::ORM::User

--- lib/Catalyst/Authentication/Store/Fey/ORM/User.pm
+++ lib/Catalyst/Authentication/Store/Fey/ORM/User.pm
@@ -16,8 +16,17 @@ sub new {
         _roles => undef,
         _user  => undef,
     };
+    bless $self, $class;
 
-    return bless $self, $class;
+    Catalyst::Exception->throw(
+        'Did you set user_model correctly?'
+    ) unless $self->config->{user_model};
+
+    Catalyst::Exception->throw(
+        'Did you set id_field correctly?'
+    ) unless $self->config->{id_field};
+
+    return $self;
 }
 
 sub supported_features {
@@ -35,7 +44,9 @@ sub load {
     #
     # search with $authinfo then set $self->_user
     #
-    #...
+    my $class = $self->config->{user_model};
+    my $user = $class->new( %$authinfo );
+    $self->_user($user);
 
     return $self->get_object ? $self : undef;
 }
@@ -53,7 +64,25 @@ sub roles {
     }
     elsif (exists($self->config->{role_relation})) {
         my $relation = $self->config->{role_relation};
-        # ...
+        my $iter = $self->_user->$relation;
+        if ($iter) {
+            while ( my $role = $iter->next ) {
+                if (my $method = $role->can($self->config->{role_field})) {
+                    push @roles, $role->$method;
+                }
+                else {
+                    Catalyst::Exception->throw(
+                        'Did you set role_field correctly?'
+                    );
+                }
+            }
+        }
+        else {
+            Catalyst::Exception->throw(
+                'Did you set role_relation correctly?'
+            );
+        }
+        $self->_roles(\@roles);
     }
     else {
         Catalyst::Exception->throw(
@@ -67,35 +96,39 @@ sub roles {
 sub for_session {
     my $self = shift;
 
-    #my %userdata = $self->_user->get_columns;
+    my %userdata;
+    for my $column ( $self->_user->Table->columns ) {
+        my $method = $column->name;
+        $userdata{$column->name} = $self->_user->$method;
+    }
+
     return \%userdata;
 }
 
 sub from_session {
     my ( $self, $frozenuser, $c ) = @_;
 
+    if ( exists( $self->config->{'use_userdata_from_session'} )
+        && $self->config->{'use_userdata_from_session'} != 0 )
+    {
+        return $self->load( $frozenuser, $c );
+    }
+
     if (ref $frozenuser eq 'HASH') {
         return $self->load(
-            {
-                map {
-                    ($_ => $frozenuser->{$_})
-                } @{ $self->config->{id_field} }
-            },
+            { map { ($_ => $frozenuser->{$_}) } $self->config->{id_field} },
             $c,
         );
     }
 
-    return $self->load(
-        { $self->config->{id_field} => $frozenuser },
-        $c,
-    );
+    return $self->load( { $self->config->{id_field} => $frozenuser }, $c );
 }
 
 sub get {
     my ( $self, $field ) = @_;
 
-    if (my $code = $self->_user->can($field)) {
-        return $self->_user->$code;
+    if (my $method = $self->_user->can($field)) {
+        return $self->_user->$method;
     }
     else {
         return undef;
@@ -113,8 +146,8 @@ sub can {
 
     return $self->SUPER::can(@_) || do {
         my ($method) = @_;
-        if (my $code = $self->_user->can($method)) {
-            return sub { shift->_user->$code(@_) };
+        if (my $method = $self->_user->can($method)) {
+            return sub { shift->_user->$method(@_) };
         }
         else {
             return;
@@ -128,22 +161,15 @@ sub AUTOLOAD {
     (my $method) = (our $AUTOLOAD =~ /([^:]+)$/);
     return if $method eq "DESTROY";
 
-    if (my $code = $self->_user->can($method)) {
-        return $self->_user->$code(@_);
+    if (my $method = $self->_user->can($method)) {
+        return $self->_user->$method(@_);
     }
     else {
         return;
     }
 }
 
-__PACKAGE__->meta->make_immutable;
+__PACKAGE__->meta->make_immutable(inline_constructor => 0);
 
 1;
 __END__
-user_model
-id_field
-ignore_fields_in_find
-role_column
-role_field
-role_relation
-use_userdata_from_session
